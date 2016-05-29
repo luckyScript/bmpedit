14756167e3bc343c7b1d65d38b9618a7590b015a
luckyscript <main.lukai@gmail.com>
2016-05-30 01:40:53 +0800

    finish threshold filter

--- main.c
+++ main.c
@@ -3,20 +3,68 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void showImageSize(FILE *p);
 int power(int num, int exp);
-int hexToDec(char a[], int len);
+int hexToDec(int a[], int len);
+void thresholdFilter(FILE *fp, char outputName[], float filter);
+
+int width, height;
 
 int main(int argc, const char * argv[]) {
+    
     FILE *fp = NULL;
+    char outputName[100];
+    float t;
+    int oFlag, tFlag, hFlag;
+
+    fp = fopen(argv[argc-1], "rb");
+    showImageSize(fp);
+    rewind(fp);
+    for (int counter = 1; counter < argc-1; counter++) {
+        if (argv[counter][0] == '-') {
+            switch (argv[counter][1]) {
+                case 't':
+                    tFlag = 1;
+                    t = (float) atof(argv[counter + 1]);
+                    break;
+                case 'o':
+                    oFlag = 1;
+                    int len = strlen(argv[counter + 1]);
+                    for(int i = 0; i < len; i++) {
+                       outputName[i] = argv[counter + 1][i]; 
+                    }
+                    outputName[len] = '\0';
+                    break;
+                case 'h':
+                    hFlag = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    if (tFlag) {
+        if (!oFlag) {
+            thresholdFilter(fp, "out.bmp", t);
+        } else {
+            thresholdFilter(fp, outputName, t);
+
+        }
+    }
+    return 0;
+}
+
+void showImageSize(FILE *p) {
     int i = 0;
-    char c;
-    char widthArr[4], heightArr[4];
-    int width, height;
-    fp = fopen("cup.bmp", "rb");
-    fseek(fp, 0, 0);
-    while(fscanf(fp, "%c", &c) != EOF) {
+    int c;
+    int widthArr[4], heightArr[4];
+    while(1) {
         i++;
+        c = fgetc(p);
         if (i > 18 && i < 23) {
             widthArr[i-19] = c;
         } else if (i > 22 && i < 27) {
@@ -25,20 +73,64 @@ int main(int argc, const char * argv[]) {
             break;
         }
     }
-    for (int count = 0; count < 4; count++) {
-        height += heightArr[count] * power(16, 2*count);
-        width += widthArr[count] * power(16, 2*count);
-    }
-    printf("width: %d", width);
-    printf("height: %d", height);
+    width = hexToDec(widthArr, 4);
+    height = hexToDec(heightArr, 4);
+    printf("Image width: %d\n", width);
+    printf("Image height: %d\n", height);
 }
 
-int hexToDec(char a[], int len) {
+void thresholdFilter(FILE *fp, char outputName[], float filter) {
+    int red, green, blue;
+    int c;
+    int size;
+    int rowSize;
+    int i = 0;
+    // calculate row size
+    size = (4 - ((width * 3) % 4)) % 4;
+    rowSize = (width * 3) + size;
+    
+    FILE *ofp = NULL;
+    ofp = fopen(outputName,"wb");
+
+    while (1) {
+        c = fgetc(fp);
+        if (c == EOF) {
+            break;
+        }
+        if (i >= 54 && ((i - 54) % rowSize) < width * 3) {
+            i += 3;
+            blue = c;
+            green = fgetc(fp);
+            red = fgetc(fp);
+            float average = ((float) (red + green + blue))/3.0f;
+            printf("%f  ", average);
+            if (average > filter * 255) {
+                red = 255;
+                green = 255;
+                blue = 255;
+            } else {
+                red = 0;
+                green = 0;
+                blue = 0;
+            }
+            fputc(blue, ofp);
+            fputc(green, ofp);
+            fputc(red, ofp);
+        } else {
+            i++;
+            fputc(c, ofp);
+        }
+
+    }
+    fclose(ofp);
+    return;
+}
+
+int hexToDec(int a[], int len) {
     int rs = 0;
     for (int i = 0; i < len; i++ ) {
         a[i] *= power(16, i*2);
         rs += a[i];
-        printf("rs: %d\n",rs);
     }
     return rs;
 }
